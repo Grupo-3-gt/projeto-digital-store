07c41465c5235684cdbcfb686dc6e97ddccb9551
iarley1 <iarley96@gmail.com>
2024-10-17 16:39:28 -0300

    create header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,44 @@
-import "./style.css"
+import "./style.css";
+import logo from "../../assets/img/svg/logo-header.svg";
+import cartIcon from "../../assets/img/svg/mini-cart.svg";
+import { useState } from "react";
 
-function Header () {
-    return (
-        <header>
-        </header>
-    )
+function Header() {
+    const [contador, setContador] = useState(0)
+
+    console.log(contador)
+
+    function incremento () {
+        setContador(contador + 1)
+    }
+  return (
+    <header>
+      <div className="div-header">
+        <h1 className="logo">
+          <img src={logo} alt="Logo digital store" />
+        </h1>
+        <div className="div-search">
+          <input type="text" placeholder="Pesquisar produto..."/>
+          {/* <button>
+                    <img src="" alt="" />
+                </button> */}
+        </div>
+        <div className="div-buttons">
+          <a href="">Cadastre-se</a>
+          <a href="">Entrar</a>
+        </div>
+        <button onClick={incremento} className="button-cart">
+          <img src={cartIcon} alt="" />
+        </button>
+      </div>
+      <nav className="navbar">
+        <a href="">Home</a>
+        <a href="">Produtos</a>
+        <a href="">Categorias</a>
+        <a href="">Meus Pedidos</a>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
