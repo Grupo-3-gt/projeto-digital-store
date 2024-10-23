470c7b9cc9879d9cdd04b001ef865bec3f44aabe
unknown <IARLEY0699>
2024-10-23 16:55:00 -0300

    add open and close menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,25 @@
 import "./style.css";
 import logo from "../../assets/img/svg/logo-header.svg";
 import cartIcon from "../../assets/img/svg/mini-cart.svg";
+import menuIcon from "../../assets/img/svg/menu.svg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Header() {
+  const [menu, setMenu] = useState(false);
+
+  function openMenu() {
+    setMenu(!menu)
+  }
+
+  console.log(menu)
   return (
     <header>
       <div className="header-container">
         <div className="div-header">
+          <button onClick={openMenu} className="menu-button">
+            <img src={menuIcon} alt="" />
+          </button>
           <h1 className="logo">
             <img src={logo} alt="Logo digital store" />
           </h1>
@@ -25,7 +37,7 @@ function Header() {
             <img src={cartIcon} alt="" />
           </button>
         </div>
-        <nav className="navbar">
+        <nav className={menu ? "navbar-active" : "navbar-disable"}>
           <a href="">Home</a>
           <a href="">Produtos</a>
           <a href="">Categorias</a>
