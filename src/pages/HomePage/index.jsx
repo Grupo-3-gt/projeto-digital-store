804661e87be252743e0d2b0dd9bfb32500e024ca
iarley1 <iarley96@gmail.com>
2024-10-22 23:49:47 -0300

    create base structure for carousel

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,12 +2,14 @@ import Header from "../../components/Header/index";
 import Card from "../../Components/ProductCard/index";
 import Footer from "../../components/Footer/index"
 import "./style.css";
+import CarouselComponent from "../../components/Carousel";
 
 function HomePage() {
   return (
     <div>
       <Header />
       <main>
+        <CarouselComponent />
         <Card />
       </main>
       <Footer />
