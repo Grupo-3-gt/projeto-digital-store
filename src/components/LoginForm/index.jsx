7b85243b43b03fafc60ee6ebd287bda8a82edd70
gisele-creat <giselesj17@gmail.com>
2024-10-26 19:03:43 -0300

    create login page

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -45,33 +45,6 @@ function LoginForm() {
         <img src={tenisLogin1} alt="" />
         <img src={tenisLogin} alt="" />
       </div>
-      {/* <div className="LoginPageImage"> 
-            <img src={tenisLogin1} alt="" />
-            <img src={tenisLogin} alt="" />
-          </div>
-        <div className="login-box">
-             <h2 className="access-text">Acesse sua conta</h2>
-             <p className="register-text">Novo cliente? Então registre-se <Link>aqui.</Link></p> */}
-      {/* <form className="form-content">
-              <div>
-                  <label htmlFor="login" className="login-label">Login *</label>
-                  <input type="text" id="login" className="login-input" placeholder="Insira seu login ou email"/>
-                </div>
-              <div>
-                 <label htmlFor="password" className="login-label">Senha *</label>
-                 <input type="password" id="password" className="login-input" placeholder="Insira sua senha" />
-               </div>
-               <div>
-                <Link className="recover-password">Esqueci minha senha</Link>
-               </div>
-              <button type="submit" className="access-button">Acessar conta</button>
-         </form>
-           <div className="login-icons">
-             <p>Ou faça login com</p>
-             <Link> <img src={IconGmail} alt="" /></Link>
-             <Link> <img src={IconFacebook} alt="" /></Link>
-           </div> */}
-      {/* </div> */}
     </div>
   );
 }
