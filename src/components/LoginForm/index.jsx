6116ac7708e2e8b9044d2add21c45ce00804e855
gisele-creat <giselesj17@gmail.com>
2024-10-26 18:47:27 -0300

    create login page

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,45 +1,79 @@
-import IconGmail from "../../assets/img/png-jpeg/gmail-login.png"
-import IconFacebook from "../../assets/img/png-jpeg/Vector.png"
-import tenisLogin1 from "../../assets/img/png-jpeg/tenisLogin1.png"
-import tenisLogin from "../../assets/img/png-jpeg/tenisLogin.png"
-import { Link } from "react-router-dom"
+import "./style.css";
+import IconGmail from "../../assets/img/png-jpeg/gmail-login.png";
+import IconFacebook from "../../assets/img/png-jpeg/Vector.png";
+import tenisLogin1 from "../../assets/img/png-jpeg/tenisLogin1.png";
+import tenisLogin from "../../assets/img/png-jpeg/tenisLogin.png";
+import { Link } from "react-router-dom";
 
-
-function LoginPage(){
-    return(
-        <div className="container-login">
-          <div> 
+function LoginForm() {
+  return (
+    <div className="login-container">
+      <form className="form-content">
+        <h2 className="access-text">Acesse sua conta</h2>
+        <p className="register-text">
+          Novo cliente? Então registre-se <Link>aqui.</Link>
+        </p>
+        <div className="input-box">
+          <label htmlFor="login">Login *</label>
+          <input
+            type="text"
+            id="login"
+            placeholder="Insira seu login ou email"
+          />
+        </div>
+        <div className="input-box">
+          <label htmlFor="password">Senha *</label>
+          <input type="password" id="password" placeholder="Insira sua senha" />
+        </div>
+        <Link className="recover-password">Esqueci minha senha</Link>
+        <button type="submit" className="access-button">
+          Acessar conta
+        </button>
+        <div className="login-icons">
+          <p>Ou faça login com</p>
+          <div>
+            <Link>
+              <img src={IconGmail} alt="" />
+            </Link>
+            <Link>
+              <img src={IconFacebook} alt="" />
+            </Link>
+          </div>
+        </div>
+      </form>
+      <div className="login-page-image">
+        <img src={tenisLogin1} alt="" />
+        <img src={tenisLogin} alt="" />
+      </div>
+      {/* <div className="LoginPageImage"> 
             <img src={tenisLogin1} alt="" />
             <img src={tenisLogin} alt="" />
           </div>
-        <div className="box-login">
-             <h2 className="">Acesse sua conta</h2>
-             <p>Novo cliente? Então <Link>registre-se aqui</Link>.</p>
-          <form>
+        <div className="login-box">
+             <h2 className="access-text">Acesse sua conta</h2>
+             <p className="register-text">Novo cliente? Então registre-se <Link>aqui.</Link></p> */}
+      {/* <form className="form-content">
               <div>
-                  <label htmlFor="login">Login *</label>
-                  <input type="text" id="login" placeholder="Insira seu login ou email"/>
+                  <label htmlFor="login" className="login-label">Login *</label>
+                  <input type="text" id="login" className="login-input" placeholder="Insira seu login ou email"/>
                 </div>
               <div>
-                 <label htmlFor="password">Senha *</label>
-                 <input type="password" id="password" placeholder="Insira sua senha" />
+                 <label htmlFor="password" className="login-label">Senha *</label>
+                 <input type="password" id="password" className="login-input" placeholder="Insira sua senha" />
                </div>
                <div>
-                <Link>Esqueci minha senha</Link>
+                <Link className="recover-password">Esqueci minha senha</Link>
                </div>
-              <button type="submit" className="">Acessar conta</button>
+              <button type="submit" className="access-button">Acessar conta</button>
          </form>
-           <p>Ou faça login com</p>
-           <div>
+           <div className="login-icons">
+             <p>Ou faça login com</p>
              <Link> <img src={IconGmail} alt="" /></Link>
              <Link> <img src={IconFacebook} alt="" /></Link>
-           </div>
-        </div>
-     
-        </div>
-
-    )
-
+           </div> */}
+      {/* </div> */}
+    </div>
+  );
 }
 
-export default LoginPage;
+export default LoginForm;
--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,13 +1,15 @@
 import Header from "../../components/Header";
 import LoginForm from "../../components/LoginForm/index";
-
+import Footer from "../../components/Footer/index"
+import "./style.css"
 function LoginPage() {
   return (
     <div>
       <Header/>
-      <main>
+      <main className="main-loginpage">
         <LoginForm/>
       </main>
+      {/* <Footer/> */}
     </div>
   );
 }
