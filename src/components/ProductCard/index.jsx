1c995f1de45bb9e2d4f314c0776386679ea65d6a
iarley1 <iarley96@gmail.com>
2024-11-07 16:23:28 -0300

    animation: slideTop 0.3s ease;

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -4,6 +4,7 @@ import arrowRight from "../../assets/img/svg/Line.svg";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { ProductContext } from "../../contexts/ProductsContext";
+import listIcon from "../../assets/img/png-jpeg/listIcon.png";
 
 function Card({ itemNum }) {
   const location = useLocation();
@@ -13,7 +14,7 @@ function Card({ itemNum }) {
 
   function clickCard(id) {
     window.scrollTo({
-      top: 0,
+      top: 100,
       behavior: "smooth",
     });
     navigate(`/products/${id}`);
@@ -29,56 +30,58 @@ function Card({ itemNum }) {
     );
   });
 
-  // filterProductsArr.length !== 0
-  //   ? listProducts.filter((product) =>
-  //       filterProductsArr.includes(product.marca.toLowerCase())
-  //     )
-  //   :
-
-  // const filteredProductData =
-  //   location.pathname === "/" ? listProducts.slice(0, 8) : listProducts;
-
-  return (
-    <section className="container-cards">
-      {location.pathname !== "/products" &&
-      location.pathname !== "/products/" ? (
-        <div className="trending-products">
-          <h3 className="text-trendin-gproducts">Produtos em alta</h3>
-          <Link className="text-see-all" to="/products">
-            Ver todos <img src={arrowRight} alt="" />
-          </Link>
-        </div>
-      ) : (
-        ""
-      )}
-      <ul className="card-list">
-        {filteredProduct.map((product) => (
-          <li
-            onClick={() => clickCard(product.id)}
-            key={product.id}
-            className="card-box"
-          >
-            <div className="image-box">
-              <span className="discount">30% OFF</span>
-              <img
-                src={product.imagem_url}
-                alt="Tênis K-Swiss V8"
-                className="image"
-              />
-            </div>
-            <div className="card-content">
-              <p className="tenis-p">{product.marca}</p>
-              <h2 className="tenis-name">{product.nome}</h2>
-              <div className="price-section">
-                <span className="old-price">{product.preco_original}</span>
-                <span className="new-price">{product.preco_desconto}</span>
+  if (filteredProduct.length !== 0) {
+    return (
+      <section className="container-cards">
+        {location.pathname !== "/products" &&
+        location.pathname !== "/products/" ? (
+          <div className="trending-products">
+            <h3 className="text-trendin-gproducts">Produtos em alta</h3>
+            <Link className="text-see-all" to="/products">
+              Ver todos <img src={arrowRight} alt="" />
+            </Link>
+          </div>
+        ) : (
+          ""
+        )}
+        <ul className="card-list">
+          {filteredProduct.map((product) => (
+            <li
+              onClick={() => clickCard(product.id)}
+              key={product.id}
+              className="card-box"
+            >
+              <div className="image-box">
+                <span className="discount">30% OFF</span>
+                <img
+                  src={product.imagem_url}
+                  alt="Tênis K-Swiss V8"
+                  className="image"
+                />
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
+              <div className="card-content">
+                <p className="tenis-p">{product.marca}</p>
+                <h2 className="tenis-name">{product.nome}</h2>
+                <div className="price-section">
+                  <span className="old-price">{product.preco_original}</span>
+                  <span className="new-price">{product.preco_desconto}</span>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  } else {
+    return (
+      <div className="not-found">
+        <div>
+          <img src={listIcon} alt="" />
+          <h2>Nenhum produto encontrado</h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Card;
