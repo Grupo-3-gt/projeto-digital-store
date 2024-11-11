9f90d1cc9ed24d95e9edc166bae2d4274183380c
Lucas Guedes <111436983+flsguedes@users.noreply.github.com>
2024-11-10 23:07:25 -0300

    feat: criando resumo de compra do cart page

---
+++ src/components/Cart/index.jsx
@@ -0,0 +1,47 @@
+import { useContext, useEffect } from "react";
+import "./style.css";
+import { ProductContext } from "../../contexts/ProductsContext";
+
+function Cart() {
+  const { cartArr, setCartArr } = useContext(ProductContext);
+
+  useEffect(() => {
+    if (localStorage.getItem("cart") !== "[]") {
+      setCartArr(JSON.parse(localStorage.getItem("cart") || "[]"));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartArr));
+  }, [cartArr]);
+
+  console.log(cartArr);
+  return (
+    <div className="cart-container">
+      <div className="box-cart">
+        <h2>MEU CARRINHO</h2>
+        <hr/>
+        <hr/>
+        <div className="form-cart">
+        <label htmlFor="voucher">Cupom de desconto *</label>
+        <input type="text" id="voucher" placeholder="Insira seu código" required/>
+        <label htmlFor="freight">Calcular frete *</label>
+        <input type="text" id="freight" placeholder="Insira seu CEP" required />
+        </div>
+      </div>
+
+      <div className="resume-cart">
+        <h2>RESUMO</h2>
+        <hr/>
+        <div className="resume-price">
+        <p>Subtotal: <span>R$219,00</span></p>
+        <p>Frete: <span>R$0,00</span></p>
+        <p>Desconto: <span>R$30,00</span></p>
+        <h3>Total <span>R$ 219,00</span></h3> 
+        </div>
+        <button className="btn-resume-cart">Continuar</button>
+      </div>
+    </div>
+  );
+}
+export default Cart;
--- src/pages/CartPage/index.jsx
+++ src/pages/CartPage/index.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
 import "./style.css"
+import Cart from "../../components/Cart";
+import Header from "../../components/Header";
+import Card from "../../components/ProductCard";
+import Footer from "../../components/Footer";
 
 function CartPage () {
+        useEffect(() => {
+          window.scrollTo(0, 0);
+        }, [])
+    
     return (
-        <h1>pagina do carrinho</h1>
+        <div>
+      <Header />
+      <main>
+        <Cart />
+        <Card itemNum={4} />
+      </main>
+      <Footer/>
+    </div>
     )
 }
 
