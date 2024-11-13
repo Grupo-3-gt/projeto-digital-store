7b3ca5ce696f690b4a83847bf7376bfbccaa5b60
MyrelaSFerreira <myrelaferreira01@gmail>
2024-11-13 16:58:46 -0300

    add sucesspage css

--- src/components/SucessInfo/index.jsx
+++ src/components/SucessInfo/index.jsx
@@ -1,11 +1,10 @@
-import  SucessFig from "../../assets/img/png-jpeg/SucessFig.png"
-
+import  SucessFig from "../../assets/img/png-jpeg/SucessFig.png";
 import React from 'react';
 import './style.css';
 
 function SucessInfo() {
   // Mock dos dados
-  const purcheasedata = {
+  const dadosCompra = {
     nome: 'Francisco Antonio Pereira',
     cpf: '123.485.913-35',
     email: 'francisco@gmail.com',
@@ -29,34 +28,41 @@ function SucessInfo() {
 
   return (
     <div className="confirm-purchease">
-      <h2>Compra Realizada com sucesso!</h2>
+      <img
+       src={SucessFig}
+       className="sucessfig"
+      />
+      <h2 className="confirm-text">Compra Realizada com sucesso!</h2>
+      <hr />
       
       <div className="information">
-        <h3>Informações Pessoais</h3>
+        <h3 className="personal-info">Informações Pessoais</h3>
         <p><strong>Nome:</strong> {dadosCompra.nome}</p>
         <p><strong>CPF:</strong> {dadosCompra.cpf}</p>
         <p><strong>Email:</strong> {dadosCompra.email}</p>
         <p><strong>Celular:</strong> {dadosCompra.celular}</p>
-
+      <hr />
         
-        <h3>Informações de Entrega</h3>
+        <h3 className="delivery-info">Informações de Entrega</h3>
         <p><strong>Endereço:</strong> {dadosCompra.endereco.rua}</p>
         <p><strong>Bairro:</strong> {dadosCompra.endereco.bairro}</p>
         <p><strong>Cidade:</strong> {dadosCompra.endereco.cidade}</p>
         <p><strong>CEP:</strong> {dadosCompra.endereco.cep}</p>
+        <hr />
 
-        <h3>Informações de Pagamento</h3>
+        <h3 className="payment-info">Informações de Pagamento</h3>
         <p><strong>Titular do Cartão:</strong> {dadosCompra.pagamento.titular}</p>
         <p><strong>Final:</strong> {dadosCompra.pagamento.finalCartao}</p>
       </div>
-
+      <hr />
       <div className="purchease-info">
         <h3>Resumo da Compra</h3>
         <p>{dadosCompra.produto.nome}</p>
         <p><strong>Total:</strong> R$ {dadosCompra.produto.preco.toFixed(2)}</p>
       </div>
       
-      <button onClick={() => alert('Imprimindo Recibo...')}>Imprimir Recibo</button>
+      
+      <button className="sucess-button"  onClick={() => alert('Imprimindo Recibo...')}>Voltar Para Home </button>
     </div>
   );
 }
