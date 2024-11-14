f5516ad218af0a553151182e1ee42e4334575730
unknown <FRANCISCO0817>
2024-11-14 16:52:48 -0300

    Adjust coupon and shipping

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -19,39 +19,54 @@ function Cart() {
   return (
     <div className="cart-container">
       <div className="box-cart">
-        <div className="title-box-cart">
+        {cartArr.map (item =>  (
+          <>
+           <div className="title-box-cart">
           <h2>MEU CARRINHO</h2>
-            <div className="text-box-cart">
-              <p>QUANTIDADE</p>
-                <p>UNITÁRIO</p>
-                  <p>TOTAL</p>
+          </div>
+          <div>
+          <div className="text-box-cart">
+            <p>QUANTIDADE</p>
+            <p>UNITÁRIO</p>
+            <p>TOTAL</p>
+          </div>
         </div>
-      </div>
+
+        <hr />
+        <p></p>
+        <hr />
+        <div className="forms-cart">
         
-        <hr/>
-        <p>{cartArr[0].nome}</p>
-        <hr/>
-        <div className="form-cart">
-        <label htmlFor="voucher">Cupom de desconto *</label>
-        <input type="text" id="voucher" placeholder="Insira seu código" required/>
-        <div/>
-        
-        <div className="form-cart-2">
-          <label htmlFor="freight">Calcular frete *</label>
-          <input type="text" id="freight" placeholder="Insira seu CEP" required />
+        <div className="voucher-form">
+          <label htmlFor="voucher">Cupom de desconto</label>
+          <div className="voucher-character">
+          <input type="text" id="voucher" placeholder="Insira seu código" required />
+          <button className="btn-voucher">OK</button>
+          </div>
         </div>
         
+
+          <div className="freight-form">
+            <label htmlFor="freight">Calcular frete</label>
+            <div className="freight-character">
+            <input type="text" id="freight" placeholder="Insira seu CEP" required />
+            <button className="btn-freight">OK</button>
+            </div>
+          </div>
+          
+        </div>
+        </>
+        ))}
       </div>
-    </div>
 
       <div className="resume-cart">
         <h2>RESUMO</h2>
-        <hr/>
+        <hr />
         <div className="resume-price">
-        <p>Subtotal: <span>R$219,00</span></p>
-        <p>Frete: <span>R$0,00</span></p>
-        <p>Desconto: <span>R$30,00</span></p>
-        <h3>Total <span>R$ 219,00</span></h3> 
+          <p>Subtotal: <span>R$219,00</span></p>
+          <p>Frete: <span>R$0,00</span></p>
+          <p>Desconto: <span>R$30,00</span></p>
+          <h3>Total <span>R$ 219,00</span></h3>
         </div>
         <button className="btn-resume-cart">Continuar</button>
       </div>
