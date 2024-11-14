bb39f3e40c3525d170a3961eccda39df776e8e7f
MyrelaSFerreira <myrelaferreira01@gmail>
2024-11-14 16:52:15 -0300

    css sucesspage 2

--- src/components/SucessInfo/index.jsx
+++ src/components/SucessInfo/index.jsx
@@ -1,3 +1,4 @@
+import  TenisMiniIcon from "../../assets/img/png-jpeg/tenissucessicon.png"
 import  SucessFig from "../../assets/img/png-jpeg/SucessFig.png";
 import React from 'react';
 import './style.css';
@@ -35,28 +36,34 @@ function SucessInfo() {
       <h2 className="confirm-text">Compra Realizada com sucesso!</h2>
       <hr />
       
-      <div className="information">
-        <h3 className="personal-info">Informações Pessoais</h3>
+      <div className="personal-info">
+        <h3>Informações Pessoais</h3>
         <p><strong>Nome:</strong> {dadosCompra.nome}</p>
         <p><strong>CPF:</strong> {dadosCompra.cpf}</p>
         <p><strong>Email:</strong> {dadosCompra.email}</p>
         <p><strong>Celular:</strong> {dadosCompra.celular}</p>
+      </div>
       <hr />
-        
-        <h3 className="delivery-info">Informações de Entrega</h3>
+      <div className="delivery-info">
+        <h3>Informações de Entrega</h3>
         <p><strong>Endereço:</strong> {dadosCompra.endereco.rua}</p>
         <p><strong>Bairro:</strong> {dadosCompra.endereco.bairro}</p>
         <p><strong>Cidade:</strong> {dadosCompra.endereco.cidade}</p>
         <p><strong>CEP:</strong> {dadosCompra.endereco.cep}</p>
-        <hr />
-
-        <h3 className="payment-info">Informações de Pagamento</h3>
+      </div>
+      <hr />
+      <div className="payment-info">
+        <h3 >Informações de Pagamento</h3>
         <p><strong>Titular do Cartão:</strong> {dadosCompra.pagamento.titular}</p>
         <p><strong>Final:</strong> {dadosCompra.pagamento.finalCartao}</p>
       </div>
       <hr />
       <div className="purchease-info">
         <h3>Resumo da Compra</h3>
+        <img
+       src={TenisMiniIcon}
+       className="tenisminiicon"
+        />
         <p>{dadosCompra.produto.nome}</p>
         <p><strong>Total:</strong> R$ {dadosCompra.produto.preco.toFixed(2)}</p>
       </div>
